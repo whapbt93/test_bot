e384f5f9c91efd4721e8ecc43605b961da2f5a77
Sergey Barancev <whapbt932@yandex.ru>
2019-05-14 12:20:47 +0300

    bot

---
+++ index.js
@@ -0,0 +1,9 @@
+const TelegramBot = require('node-telegram-bot-api')
+
+const TOKEN = '875721491:AAFDUCjWsjL-NQSgkJdd4CtcrAcRdPwMhuA'
+
+const bot = new TelegramBot(TOKEN, {polling: true})
+
+bot.on('message', msg => {
+    bot.sendMessage(msg.chat.id, 'Hello, ${msg.from.first_name}, ti Pidor')
+})
