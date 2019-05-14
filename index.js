c62f55ccefc407832e3a4aeb052bfbb14a511015
whapbt93 <50616283+whapbt93@users.noreply.github.com>
2019-05-14 13:19:32 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const TOKEN = '875721491:AAFDUCjWsjL-NQSgkJdd4CtcrAcRdPwMhuA'
 const bot = new TelegramBot(TOKEN, {polling: true})
 
 bot.on('message', msg => {
-    bot.sendMessage(msg.chat.id, 'Hello, ${msg.from.first_name}, ti Pidor')
+    bot.sendMessage(msg.chat.id, 'Hello, ${msg.from.first_name}, ti Pidor' ${msg.from.first_name} )
 })
