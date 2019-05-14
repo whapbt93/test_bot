aafaa25e75787947f6c6805ec0ce98b62197f96d
whapbt93 <50616283+whapbt93@users.noreply.github.com>
2019-05-14 16:49:04 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ require('http').createServer().listen(process.env.PORT || 5000).on('request', fu
     res.end('')
 })
 
+/*
 bot.on('message', msg => {
     bot.sendMessage(msg.chat.id, 'Привет, ' + msg.from.first_name )
 })
@@ -15,3 +16,26 @@ bot.on('message', msg => {
 bot.onText(/\/start/, msg=> {
     bot.sendMessage(msg.chat.id, 'Добро пожаловать!')
 })
+
+ */
+
+var instr
+
+function OutMsg(instr) {
+    if (instr[0] != '/') {
+        bot.sendMessage(msg.chat.id, 'Привет')
+    } else {
+        switch (instr) {
+            case '/start':
+                bot.sendMessage(msg.chat.id, 'Добро пожаловать')
+                break
+            case '/help':
+                bot.sendMessage(msg.chat.id, 'Я умею:')
+                break
+            default:
+                bot.sendMessage(msg.chat.id, 'Неизвестная команда')
+        }
+    }
+}
+
+bot.onText(instr, OutMsg(instr))
