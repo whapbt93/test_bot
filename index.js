14f0bc4a56e68ed9cf77d369117d2fbd7941d155
whapbt93 <50616283+whapbt93@users.noreply.github.com>
2019-05-14 12:46:38 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+require('http').createServer().listen(process.env.PORT || 5000).on('request', function(req, res){
+    res.end('')
+})﻿
+
 const TelegramBot = require('node-telegram-bot-api')
 
 const TOKEN = '875721491:AAFDUCjWsjL-NQSgkJdd4CtcrAcRdPwMhuA'
@@ -5,5 +9,5 @@ const TOKEN = '875721491:AAFDUCjWsjL-NQSgkJdd4CtcrAcRdPwMhuA'
 const bot = new TelegramBot(TOKEN, {polling: true})
 
 bot.on('message', msg => {
-    bot.sendMessage(msg.chat.id, 'Hello, ${msg.from.first_name}, ti Pidor')
+    bot.sendMessage(msg.chat.id, 'Hello, ${msg.from.first_name}, ti Pidor' ${msg.from.first_name})
 })
